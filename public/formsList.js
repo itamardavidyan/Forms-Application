dcf174a635fe927538126cc91ede6e48cdf64fde
Itamar Davidyan <itamardavidyan@gmail.com>
2018-08-24 13:08:14 +0300

    newtry

--- public/formsList.js
+++ public/formsList.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-});
 
-$(".move2formBuilder").click(function(){
-	const express = require('express');
-	const app = express();
+	$(".move2formBuilder").click(function(){
+		const express = require('express');
+		const app = express();
 
-	app.use(express.static(__dirname + '/public'));
+		app.use(express.static(__dirname + '/public'));
 
-	app.get("/", (req,res) =>{
-		res.sendfile(__dirname + '/formBuilder/formBuilder.html');
+		app.get("/", (req,res) =>{
+			res.sendfile(__dirname + '/formBuilder/formBuilder.html');
+		});
 	});
 });
