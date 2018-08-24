760f7b92064d08110091b91c5d65d86f98c66598
Itamar Davidyan <itamardavidyan@gmail.com>
2018-08-24 14:20:59 +0300

    newtry

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ app.get("/", (req,res) =>{
 	res.sendfile(__dirname + '/index.html');
 });
 
-app.route('/goData')
+app.route('/formBuilder')
  	.get(function (req, res) {
 		res.sendFile(path  + '/public/formBuilder/formBuilder.html');
+});
+
+app.route('/formList')
+ 	.get(function (req, res) {
+		res.sendFile(path  + '/public/index.html');
 });
 
 const PORT = process.env.PORT || 3000;
