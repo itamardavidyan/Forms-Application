32281713cf465c8380a40fcf2a48e92ec8c142bf
Itamar Davidyan <itamardavidyan@gmail.com>
2018-08-24 12:20:14 +0300

    newtry

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", (req,res) =>{
-	res.send({deploy:'your site'})
+	res.sendfile(__dirname + '/index.html');
+	// res.send({deploy:'your site'})
 });
 
 const PORT = process.env.PORT || 3000;
